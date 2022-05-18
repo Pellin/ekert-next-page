b53d2b0ae1a3103fa12ff46eebdb15fc1ba0405c
PelleLindroth <pellelindroth@gmail.com>
2022-05-18 09:34:42 +0200

    Extract VideoCard to own component

---
+++ components/admin/VideoSection/VideoList/VideoCard.tsx
@@ -0,0 +1,51 @@
+import Image from 'next/image'
+import React, { useContext, useRef, useState } from 'react'
+import { AdminContext } from '../../../../contexts/admin/AdminContext'
+import { IVideo } from '../../../../globalTypes'
+import styles from './VideoList.module.scss'
+
+const VideoCard = ({ video }: { video: IVideo }) => {
+  const { refreshVideoUrl } = useContext(AdminContext)!
+  const videoRef = useRef<HTMLVideoElement>(null)
+  const [currentUrl, setCurrentUrl] = useState(video.signedUrl)
+  const [isUpdating, setIsUpdating] = useState(false)
+
+  const refetchUrl = async (title: string) => {
+    setIsUpdating(true)
+
+    const newUrl = await refreshVideoUrl(title)
+
+    if (videoRef.current) {
+      setCurrentUrl(newUrl)
+      setIsUpdating(false)
+    }
+  }
+
+  return (
+    <li className={styles.videoCard}>
+      <div className={styles.cardHeader}>
+        <h3>{video.title}</h3>
+        <div className={styles.iconWrapper}>
+          <Image
+            title="Inspektera"
+            src="/icons/info-icon.png"
+            alt="Inspektera"
+            width={16}
+            height={16}
+          />
+        </div>
+      </div>
+      <video
+        ref={videoRef}
+        onContextMenu={(e) => e.preventDefault()}
+        controls
+        controlsList="nodownload"
+        src={currentUrl}
+        onError={() => refetchUrl(video.title)}
+      />
+      {isUpdating && <p>Uppdaterar video...</p>}
+    </li>
+  )
+}
+
+export default VideoCard
--- components/admin/VideoSection/VideoList/VideoList.tsx
+++ components/admin/VideoSection/VideoList/VideoList.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
-import React, { useContext } from 'react'
+import React, { useContext, useRef, useState } from 'react'
 import { AdminContext } from '../../../../contexts/admin/AdminContext'
+import VideoCard from './VideoCard'
 import styles from './VideoList.module.scss'
 
 const VideoList = () => {
@@ -9,26 +10,7 @@ const VideoList = () => {
   return (
     <ul className={styles.videoList}>
       {videos.map((video) => (
-        <li key={video._id!} className={styles.videoCard}>
-          <div className={styles.cardHeader}>
-            <h3>{video.title}</h3>
-            <div className={styles.iconWrapper}>
-              <Image
-                title="Inspektera"
-                src="/icons/info-icon.png"
-                alt="Inspektera"
-                width={16}
-                height={16}
-              />
-            </div>
-          </div>
-          <video
-            onContextMenu={(e) => e.preventDefault()}
-            controls
-            controlsList="nodownload"
-            src={video.signedUrl}
-          />
-        </li>
+        <VideoCard key={video._id} video={video} />
       ))}
     </ul>
   )
