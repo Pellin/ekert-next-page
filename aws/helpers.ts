219e84c14eb88d2c04b5a046eec52affd6e237b9
PelleLindroth <pellelindroth@gmail.com>
2022-05-18 09:35:51 +0200

    Update file upload to s3

--- aws/helpers.ts
+++ aws/helpers.ts
@@ -18,37 +18,42 @@ export const getSignedVideoUrl = async (key: string) => {
 }
 
 export const uploadFileToS3 = async (file: FileData) => {
-  const fileType = getFileType(file.mimetype)
+  try {
+    const fileType = getFileType(file.mimetype)
 
-  let key = `${fileType}/${file.name}`
+    let key = `${fileType}/${file.name}`
 
-  if (process.env.NODE_ENV === 'development') {
-    key = `test/${fileType}/${file.name}`
-  }
-
-  const params = {
-    Bucket: S3_BUCKET!,
-    Key: key,
-    Body: file.data,
-    Encoding: file.encoding,
-    ContentType: file.mimetype,
-    ACL: 'public-read',
-  }
-
-  let uploadedThumbnail: ManagedUpload.SendData | null = null
-
-  const uploadedFile = await s3.upload(params).promise()
-  if (fileType === 'images') {
-    uploadedThumbnail = await createAndUploadThumbnailToS3(file)
-  }
-
-  if (uploadedFile) {
-    return {
-      url: uploadedFile.Location,
-      thumbnail: uploadedThumbnail?.Location,
+    if (process.env.NODE_ENV === 'development') {
+      key = `test/${fileType}/${file.name}`
     }
-  } else {
-    return false
+
+    const params = {
+      Bucket: S3_BUCKET!,
+      Key: key,
+      Body: file.data,
+      Encoding: file.encoding,
+      ContentType: file.mimetype,
+      ACL: fileType === 'images' ? 'public-read' : 'private',
+    }
+
+    let uploadedThumbnail: ManagedUpload.SendData | null = null
+
+    const uploadedFile = await s3.upload(params).promise()
+
+    if (fileType === 'images') {
+      uploadedThumbnail = await createAndUploadThumbnailToS3(file)
+    }
+
+    if (uploadedFile) {
+      return {
+        url: uploadedFile.Location,
+        thumbnail: uploadedThumbnail?.Location,
+      }
+    } else {
+      return false
+    }
+  } catch (error) {
+    console.log(error)
   }
 }
 
