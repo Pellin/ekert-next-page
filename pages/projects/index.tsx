bc6f59350d49feb18524a4bb619f53dec37cae6f
PelleLindroth <pellelindroth@gmail.com>
2022-05-19 09:35:47 +0200

    Remove /projects route

--- pages/projects/index.tsx
+++
@@ -1,9 +0,0 @@
-import { NextPage } from 'next'
-import React from 'react'
-
-// renders all projects
-const AllProjectsPage: NextPage = () => {
-  return <div>All Projects Page</div>
-}
-
-export default AllProjectsPage
