3e07f5244c89fd81913ab52661f7d62f07aab091
PelleLindroth <pellelindroth@gmail.com>
2022-05-23 18:59:13 +0200

    Show associated projects in LightBox

--- components/admin/ImageSection/Lightbox/Lightbox.tsx
+++ components/admin/ImageSection/Lightbox/Lightbox.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
-import React, { SyntheticEvent, useState } from 'react'
-import { IImage } from '../../../../globalTypes'
+import React, { SyntheticEvent, useContext, useEffect, useState } from 'react'
+import { AdminContext } from '../../../../contexts/admin/AdminContext'
+import { IImage, IProject } from '../../../../globalTypes'
 import styles from './Lightbox.module.scss'
 
 type LightBoxProps = {
@@ -10,9 +11,17 @@ type LightBoxProps = {
 }
 
 const Lightbox = ({ image, visible, setShowLightbox }: LightBoxProps) => {
+  const { projects } = useContext(AdminContext)!
+  const [associatedProjects, setAssociatedProjects] = useState<IProject[]>([])
   const [naturalHeight, setNaturalHeight] = useState(400)
   const [naturalWidth, setNaturalWidth] = useState(400)
   const [isLandscape, setIsLandscape] = useState(false)
+
+  useEffect(() => {
+    setAssociatedProjects(
+      projects.filter((project) => project.images.includes(image?._id!))
+    )
+  }, [projects, image?._id!])
 
   const getDimensions = (e: SyntheticEvent<HTMLImageElement>) => {
     setIsLandscape(e.currentTarget.naturalWidth > e.currentTarget.naturalHeight)
@@ -21,17 +30,48 @@ const Lightbox = ({ image, visible, setShowLightbox }: LightBoxProps) => {
   }
 
   if (!image) return null
-  console.log(image.size)
 
   const imageSize = `${(
     image.size / (image.size > 1000000 ? 1000000 : 1000)
   ).toFixed(1)} ${image.size > 1000000 ? 'MB' : 'KB'}`
 
+  const handleRemoveImageFromProject = (projectId: string) => {
+    setAssociatedProjects((prev) =>
+      // call context removeImageFromProject
+      prev.filter((project) => project._id !== projectId)
+    )
+  }
+
   return (
     <div className={visible ? styles.lightbox : styles.hidden}>
       <aside className={styles.infoBox}>
-        <h3>{image.title}</h3>
+        <h2>{image.title}</h2>
         <h4>{imageSize}</h4>
+        <h3>I projekt:</h3>
+        {associatedProjects.length ? (
+          <ul className={styles.associatedProjects}>
+            {associatedProjects.map((project) => (
+              <li className={styles.projectRow} key={project._id}>
+                <p>{project.title}</p>
+                <div
+                  onClick={() => handleRemoveImageFromProject(project._id!)}
+                  title="Ta bort från projekt"
+                  className={styles.minusIconWrapper}
+                >
+                  <Image
+                    src="/icons/circle-minus-icon-white.png"
+                    alt="Ta bort från projekt"
+                    height={14}
+                    width={14}
+                    layout="responsive"
+                  />
+                </div>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>Inga associerade projekt</p>
+        )}
       </aside>
       <div className={styles.imageContainer}>
         <div
