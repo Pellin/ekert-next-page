d8691e92108accc5bfbb2795bf4eabbc35d3a501
PelleLindroth <pellelindroth@gmail.com>
2022-05-25 18:08:58 +0200

    Links in Home Project section

--- components/home/ProjectSection/ProjectSection.tsx
+++ components/home/ProjectSection/ProjectSection.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 import styles from './ProjectSection.module.scss'
 
@@ -8,45 +9,51 @@ const ProjectSection = () => {
       <h2>Projekt i urval</h2>
       <ul className={styles.projectGrid}>
         <li className={styles.projectCard}>
-          <div className={styles.cardImageWrapper}>
-            <Image
-              src="/images/trolljägarna.png"
-              alt="Trolljägarna - Robert Achberg"
-              width={'100%'}
-              height={'100%'}
-              objectFit="contain"
-              layout="responsive"
-            />
-            <div className={styles.gradient}></div>
-          </div>
+          <Link href="/projekt/trolljagarna">
+            <div className={styles.cardImageWrapper}>
+              <Image
+                src="/images/trolljägarna.png"
+                alt="Trolljägarna - Robert Achberg"
+                width={'100%'}
+                height={'100%'}
+                objectFit="contain"
+                layout="responsive"
+              />
+              <div className={styles.gradient}></div>
+            </div>
+          </Link>
           <h3 className={styles.red}>TROLLJÄGARNA</h3>
         </li>
         <li className={styles.projectCard}>
-          <div className={styles.cardImageWrapper}>
-            <Image
-              src="/images/klimatkampen.jpeg"
-              alt="Klimatkampen"
-              width={'100%'}
-              height={'100%'}
-              objectFit="cover"
-              layout="responsive"
-            />
-            <div className={styles.gradient}></div>
-          </div>
+          <Link href="/projekt/klimatkampen">
+            <div className={styles.cardImageWrapper}>
+              <Image
+                src="/images/klimatkampen.jpeg"
+                alt="Klimatkampen"
+                width={'100%'}
+                height={'100%'}
+                objectFit="cover"
+                layout="responsive"
+              />
+              <div className={styles.gradient}></div>
+            </div>
+          </Link>
           <h3 className={styles.green}>KLIMATKAMPEN</h3>
         </li>
         <li className={styles.projectCard}>
-          <div className={styles.cardImageWrapper}>
-            <Image
-              src="/images/perspektiv-på-världen.jpeg"
-              alt="Perspektiv på världen"
-              width={'100%'}
-              height={'100%'}
-              objectFit="cover"
-              layout="responsive"
-            />
-            <div className={styles.gradient}></div>
-          </div>
+          <Link href="/projekt/perspektiv-pa-varlden">
+            <div className={styles.cardImageWrapper}>
+              <Image
+                src="/images/perspektiv-på-världen.jpeg"
+                alt="Perspektiv på världen"
+                width={'100%'}
+                height={'100%'}
+                objectFit="cover"
+                layout="responsive"
+              />
+              <div className={styles.gradient}></div>
+            </div>
+          </Link>
           <h3>PERSPEKTIV PÅ VÄRLDEN</h3>
         </li>
       </ul>
