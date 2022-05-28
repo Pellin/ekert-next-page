c7c54540240162b1683150d9824ff1c52bbc88c6
PelleLindroth <pellelindroth@gmail.com>
2022-05-28 21:32:50 +0200

    Remove unnecessary import

--- components/admin/ImageSection/ImageSection.tsx
+++ components/admin/ImageSection/ImageSection.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import Image from 'next/image'
 import ImageList from './ImageList'
 import styles from '../../admin/AdminContent/AdminContent.module.scss'
