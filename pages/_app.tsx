c7a5bff6b5e98e0f8665d6e0aff0d52eeb574bfa
PelleLindroth <pellelindroth@gmail.com>
2022-05-31 11:39:02 +0200

    Extract Head to own component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,40 +1,16 @@
 import type { AppProps } from 'next/app'
-import Head from 'next/head'
 import { SessionProvider } from 'next-auth/react'
-import Layout from '../components/layout/Layout'
 import AdminContextProvider from '../contexts/admin/AdminContext'
+import CustomHead from '../components/global/CustomHead'
+import Layout from '../components/layout/Layout'
 import '../styles/globals.scss'
 
-const MyApp = ({ Component, pageProps }: AppProps) => {
+const App = ({ Component, pageProps }: AppProps) => {
   const { session } = pageProps
 
   return (
     <SessionProvider>
-      <Head>
-        <title>Anders Ekert Produktion</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-        <meta name="msapplication-TileColor" content="#00aba9" />
-        <meta name="theme-color" content="#f5f5f5" />
-      </Head>
+      <CustomHead />
       <Layout>
         {session ? (
           <AdminContextProvider>
@@ -48,4 +24,4 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   )
 }
 
-export default MyApp
+export default App
